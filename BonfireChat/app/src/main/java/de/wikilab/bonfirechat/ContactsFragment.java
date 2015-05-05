208ccfd6019b875f27eb11dd946e5592a1f753b2
Johannes Lauinger <johannes@lauinger-it.de>
2015-05-05 16:10:32 +0200

    fügt einen Texteingabedialog hinzu, mit dem Kontakte hinzugefügt werden können

--- BonfireChat/app/src/main/java/de/wikilab/bonfirechat/ContactsFragment.java
+++ BonfireChat/app/src/main/java/de/wikilab/bonfirechat/ContactsFragment.java
@@ -1,6 +1,8 @@
 package de.wikilab.bonfirechat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +11,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,10 +23,13 @@ import java.util.ArrayList;
  */
 public class ContactsFragment extends Fragment {
 
+    private ContactsAdapter adapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        adapter = new ContactsAdapter(this.getActivity(), new ArrayList<Contact>());
     }
 
     @Override
@@ -36,11 +43,6 @@ public class ContactsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
 
         final ListView contactsList = (ListView) rootView.findViewById(R.id.contactsList);
-        final ArrayList<Contact> contactsListItems = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            contactsListItems.add(new Contact("Simon Thiem"));
-        }
-        final ContactsAdapter adapter = new ContactsAdapter(this.getActivity(), contactsListItems);
         contactsList.setAdapter(adapter);
 
         return rootView;
@@ -57,7 +59,38 @@ public class ContactsFragment extends Fragment {
         int id = item.getItemId();
 
         if (item.getItemId() == R.id.action_add_contact) {
-            Toast.makeText(getActivity(), "Much contacts added.", Toast.LENGTH_SHORT).show();
+            final EditText input = new EditText(getActivity());
+            input.setSingleLine();
+            FrameLayout container = new FrameLayout(getActivity());
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.leftMargin = 50;
+            params.rightMargin = 50;
+            input.setLayoutParams(params);
+            container.addView(input);
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("Neuer Kontakt")
+                    .setMessage("Nach Name suchen:")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            String name = input.getText().toString();
+                            // TODO: insert sophisticated contact existing check
+                            if (!name.isEmpty()) {
+                                Contact contact = new Contact(name);
+                                adapter.add(contact);
+                            }
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(R.mipmap.ic_launcher)
+                    .setView(container)
+                    .show();
+
             return true;
         }
 
