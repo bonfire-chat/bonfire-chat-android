a89a13a15deb5d066e499c9b570cf533135c69dd
Max Weller <git@max-weller.de>
2015-06-15 23:22:28 +0200

    make EchoProtocol use Envelopes instead of Messages

--- BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/network/EchoProtocol.java
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/network/EchoProtocol.java
@@ -1,6 +1,13 @@
 package de.tudarmstadt.informatik.bp.bonfirechat.network;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import de.tudarmstadt.informatik.bp.bonfirechat.models.Contact;
+import de.tudarmstadt.informatik.bp.bonfirechat.models.Envelope;
 import de.tudarmstadt.informatik.bp.bonfirechat.models.Identity;
 import de.tudarmstadt.informatik.bp.bonfirechat.models.Message;
 
@@ -12,9 +19,19 @@ public class EchoProtocol implements IProtocol {
     OnMessageReceivedListener listener;
 
     @Override
-    public void sendMessage(Contact target, Message message) {
-        Message newMsg = new Message(message.body, Message.MessageDirection.Received, message.dateTime);
-        listener.onMessageReceived(this, newMsg);
+    public void sendMessage(Envelope envelope) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(this);
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            Envelope myNewEnvelope = (Envelope) ois.readObject();
+            listener.onMessageReceived(this, myNewEnvelope);
+        } catch (IOException e) {
+        } catch (ClassNotFoundException e) {
+        }
     }
 
     @Override
