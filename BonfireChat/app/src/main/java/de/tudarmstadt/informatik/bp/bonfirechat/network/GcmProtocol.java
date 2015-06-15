20559d2a06c449c14d665ac266edcf75d67dd758
Max Weller <git@max-weller.de>
2015-06-15 23:53:32 +0200

    start implementing simple Client Server protocol based on Google Cloud Messaging

--- BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/network/GcmProtocol.java
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/network/GcmProtocol.java
@@ -1,7 +1,19 @@
 package de.tudarmstadt.informatik.bp.bonfirechat.network;
 
+import de.tudarmstadt.informatik.bp.bonfirechat.models.Envelope;
+
 /**
  * Created by mw on 15.06.15.
  */
-public class GcmProtocol {
+public class GcmProtocol extends SocketProtocol {
+
+    @Override
+    public void sendMessage(Envelope envelope) {
+
+    }
+
+    @Override
+    public boolean canSend() {
+        return false;
+    }
 }
