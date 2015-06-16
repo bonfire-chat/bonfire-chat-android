afa1262ab5278f08545d8cfb90bcd99f97dba30c
Max Weller <git@max-weller.de>
2015-06-16 14:50:21 +0200

    hexdump helper method eingefügt

--- BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/helper/StreamHelper.java
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/helper/StreamHelper.java
@@ -1,5 +1,7 @@
 package de.tudarmstadt.informatik.bp.bonfirechat.helper;
 
+import java.util.Formatter;
+
 /**
  * Created by mw on 16.06.15.
  */
@@ -8,6 +10,14 @@ public class StreamHelper {
     public static String convertStreamToString(java.io.InputStream is) {
         java.util.Scanner s = new java.util.Scanner(is, "UTF-8").useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
+    }
+
+    public static String byteArrayToHexString(byte[] byteArray) {
+        Formatter formatter = new Formatter();
+        for (byte b : byteArray) {
+            formatter.format("%02x", b);
+        }
+        return formatter.toString();
     }
 
 }
