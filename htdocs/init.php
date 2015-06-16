3a6e28ef024d253f3e5716fd47f80fd0cb0ee99d
Max Weller <max@teamwiki.de>
2015-06-16 19:35:01 +0200

    improvements for server backend

--- htdocs/init.php
+++ htdocs/init.php
@@ -4,3 +4,17 @@ if (!$DB_USER) die("Please copy .htconfig.php.template to .htconfig.php and fill
 
 $db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8", $DB_USER, $DB_PASS);
 
+function errorResult($code, $msg) {
+  if ($code == 400) header("HTTP/1.1 400 Bad Request"); else header("HTTP/1.1 500 Internal Server Error");
+  echo $msg;
+  error_log("ERROR: $msg");
+  exit;
+}
+
+
+function base64url_decode($base64url) {
+  $base64 = strtr($base64url, '-_', '+/');
+  $plainText = base64_decode($base64);
+  return ($plainText);
+}
+
--- htdocs/search.php
+++ htdocs/search.php
@@ -3,6 +3,11 @@ require_once "init.php";
 
 header("Content-Type: application/json");
 
+if (!isset($_GET["nickname"])) {
+  header("HTTP/1.1 400 Bad Request");
+  die(json_encode(array("error" => "Bad request")));
+}
+
 $stmt = $db->prepare("SELECT nickname,xmppid,publickey,phone,last_updated FROM users WHERE nickname LIKE ? ORDER BY last_updated desc");
 $stmt->execute(array($_GET["nickname"]));
 
