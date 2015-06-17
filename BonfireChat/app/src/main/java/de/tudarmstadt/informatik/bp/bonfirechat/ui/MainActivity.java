7d853cd9904294bd1655d78d1c5498cb051e44b1
Max Weller <git@max-weller.de>
2015-06-17 13:41:10 +0200

    add QR scanning button in ShareMyIdentityActivity

--- BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/ui/MainActivity.java
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/ui/MainActivity.java
@@ -110,6 +110,9 @@ public class MainActivity extends Activity
         }
     }
 
+    /**
+     * handle results from qr code scanning
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
