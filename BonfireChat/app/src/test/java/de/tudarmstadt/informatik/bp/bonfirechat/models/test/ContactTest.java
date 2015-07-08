6bc0e7cf4480fd7402a74f856336c48a22dc451c
Jonas <jonas.moennig@stud.tu-darmstadt.de>
2015-07-08 17:11:38 +0200

    Mockito korrekt eingerichtet und ersten Test geschrieben
    toString() von Contact getestet

--- BonfireChat/app/src/test/java/de/tudarmstadt/informatik/bp/bonfirechat/models/test/ContactTest.java
+++ BonfireChat/app/src/test/java/de/tudarmstadt/informatik/bp/bonfirechat/models/test/ContactTest.java
@@ -1,19 +1,37 @@
 package de.tudarmstadt.informatik.bp.bonfirechat.models.test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import de.tudarmstadt.informatik.bp.bonfirechat.models.Contact;
+import de.tudarmstadt.informatik.bp.bonfirechat.models.MyPublicKey;
+import org.abstractj.kalium.keys.KeyPair;
 
 /**
  * Created by jonas on 30.06.15.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ContactTest {
+
+    public org.abstractj.kalium.keys.PublicKey publicKeyMock;
+    Contact contact;
+
+    @Before
+    public void initTests(){
+        KeyPair keyPairMock = Mockito.mock(KeyPair.class);
+        org.abstractj.kalium.keys.PublicKey publicKeyMock = Mockito.mock(org.abstractj.kalium.keys.PublicKey.class);
+        when(keyPairMock.getPublicKey()).thenReturn(publicKeyMock);
+        contact = new Contact("nickname", "", "", "", new MyPublicKey(publicKeyMock), "", "", "", 0);
+    }
 
     @Test
     public void testToString(){
-        Contact contact = new Contact("nickname", "", "", "", "", "", "", "", 0);
         assertEquals("nickname", contact.toString());
     }
 
