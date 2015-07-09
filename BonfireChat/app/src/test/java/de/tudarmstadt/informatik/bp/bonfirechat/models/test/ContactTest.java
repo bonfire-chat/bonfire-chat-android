c5bca0639f5b6a2272d9bb2fe2b546fefdc9cbb6
Jonas Mönnig <jonas.moennig@stud.tu-darmstadt.de>
2015-07-09 13:29:42 +0200

    Contact getter und setter getestet
    - Skript gefixt (@Test hinzugefügt)
    - Skript in tools verschoben

--- BonfireChat/app/src/test/java/de/tudarmstadt/informatik/bp/bonfirechat/models/test/ContactTest.java
+++ BonfireChat/app/src/test/java/de/tudarmstadt/informatik/bp/bonfirechat/models/test/ContactTest.java
@@ -27,12 +27,46 @@ public class ContactTest {
         KeyPair keyPairMock = Mockito.mock(KeyPair.class);
         org.abstractj.kalium.keys.PublicKey publicKeyMock = Mockito.mock(org.abstractj.kalium.keys.PublicKey.class);
         when(keyPairMock.getPublicKey()).thenReturn(publicKeyMock);
-        contact = new Contact("nickname", "", "", "", new MyPublicKey(publicKeyMock), "", "", "", 0);
+        contact = new Contact("nickname", "Nick", "Name", "", new MyPublicKey(publicKeyMock), "", "", "", 0);
     }
 
     @Test
     public void testToString(){
         assertEquals("nickname", contact.toString());
     }
+
+    @Test
+    public void testGetNickname(){
+        assertEquals("nickname", contact.getNickname());
+    }
+
+    @Test
+    public void testSetNickname(){
+        contact.setNickname("nickname2");
+        assertEquals("nickname2", contact.getNickname());
+    }
+
+    @Test
+    public void testGetFirstName(){
+        assertEquals("Nick", contact.getFirstName());
+    }
+
+    @Test
+    public void testSetFirstName(){
+        contact.setFirstName("Nick2");
+        assertEquals("Nick2", contact.getFirstName());
+    }
+
+    @Test
+    public void testGetLastName(){
+        assertEquals("Name", contact.getLastName());
+    }
+
+    @Test
+    public void testSetLastName(){
+        contact.setLastName("Name2");
+        assertEquals("Name2", contact.getLastName());
+    }
+
 
 }
