7ecc816bcf181dd061a34971eb2945f7503d60a9
Johannes Lauinger <johannes@lauinger-it.de>
2015-07-10 17:51:06 +0200

    StatsCollector als NonStopIntentService hinzugefügt, Intent zum Veröffentlichen der Statistiken via POST /api/v1/stats implementiert

---
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/stats/StatsCollector.java
@@ -0,0 +1,82 @@
+package de.tudarmstadt.informatik.bp.bonfirechat.stats;
+
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+
+import de.tudarmstadt.informatik.bp.bonfirechat.data.BonfireData;
+import de.tudarmstadt.informatik.bp.bonfirechat.helper.CryptoHelper;
+import de.tudarmstadt.informatik.bp.bonfirechat.helper.DateHelper;
+import de.tudarmstadt.informatik.bp.bonfirechat.network.NonStopIntentService;
+
+/**
+ * Created by johannes on 10.07.15.
+ */
+public class StatsCollector extends NonStopIntentService {
+
+    private static final String TAG = "StatsCollector";
+
+    // action in Intents which are sent to the service
+    public static final String PUBLISH_STATS_ACTION = "de.tudarmstadt.informatik.bp.bonfirechat.PUBLISH_STATS";
+
+    /**
+     * Creates the StatsCollector, called by Android creating the service
+     */
+    public StatsCollector() {
+        super("BonFire_StatsCollector");
+    }
+
+    /*
+     * handle stats intents
+     */
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent.getAction() == PUBLISH_STATS_ACTION) {
+            BonfireData db = BonfireData.getInstance(this);
+            StatsEntry stats = CurrentStats.getInstance();
+
+            // save latest stats data to local database
+            db.addStatsEntry(stats);
+
+            // publish stats object to the server API
+            String postData = "timestamp=" + DateHelper.formatDateTime(stats.timestamp)
+                    + "&created_at=" + DateHelper.formatDateTime(stats.created_at)
+                    + "&batterylevel=" + stats.batteryLevel
+                    + "&powerusage=" + stats.powerUsage
+                    + "&messages_sent=" + stats.messagesSent
+                    + "&messages_received=" + stats.messageReceived
+                    + "&lat=" + stats.lat
+                    + "&lng=" + stats.lng;
+
+            HttpURLConnection urlConnection = null;
+            try {
+                urlConnection = (HttpURLConnection) new URL(BonfireData.API_ENDPOINT + "/stats").openConnection();
+                urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                urlConnection.setDoOutput(true);
+                urlConnection.setChunkedStreamingMode(0);
+
+                BufferedOutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                out.write(postData.getBytes("UTF-8"));
+                out.flush();
+
+                if (urlConnection.getResponseCode() != 200) {
+                    Log.e(TAG, "error publishing stats: server sent response code " + urlConnection.getResponseCode());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(urlConnection == null) urlConnection.disconnect();
+            }
+        }
+    }
+}
