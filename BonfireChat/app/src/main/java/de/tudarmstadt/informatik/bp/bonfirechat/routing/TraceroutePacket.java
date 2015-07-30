035a916f1459fc0971f1866ced0f54044ff76cc9
Johannes Lauinger <johannes@lauinger-it.de>
2015-07-30 18:40:47 +0200

    adds foo

--- BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/routing/TraceroutePacket.java
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/routing/TraceroutePacket.java
@@ -3,5 +3,5 @@ package de.tudarmstadt.informatik.bp.bonfirechat.routing;
 /**
  * Created by johannes on 29.07.15.
  */
-public class TraceroutePacket extends ControlPacket{
+public class TraceroutePacket extends ControlPacket {
 }
