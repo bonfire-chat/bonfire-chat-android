37798660604ae97091ee6e5b0549c5911cfbdfd0
Max Weller <max@teamwiki.de>
2015-08-30 00:15:11 +0200

    throw on DB error

--- htdocs/api/v1/init.php
+++ htdocs/api/v1/init.php
@@ -1,8 +1,9 @@
 <?php
 include ".htconfig.php";
 if (!$DB_USER) die("Please copy htconfig_template.php to .htconfig.php and fill in the settings");
-
+ini_set("display_errors","on");
 $db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8", $DB_USER, $DB_PASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 function errorResult($code, $msg) {
   if ($code == 400) header("HTTP/1.1 400 Bad Request"); else header("HTTP/1.1 500 Internal Server Error");
