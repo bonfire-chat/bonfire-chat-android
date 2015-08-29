4cf3dc0fa78b971cd75fa62c9ee10a9b8a31c447
Johannes Lauinger <johannes@lauinger-it.de>
2015-08-29 03:54:40 +0200

    add rowlayout for sending in progress, add a segment to traceroute on dispatching a message

---
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/routing/TracerouteProgressSegment.java
@@ -0,0 +1,9 @@
+package de.tudarmstadt.informatik.bp.bonfirechat.routing;
+
+/**
+ * Created by johannes on 29.08.15.
+ *
+ * segment indication a message is still on its way
+ */
+public class TracerouteProgressSegment implements TracerouteSegment {
+}
