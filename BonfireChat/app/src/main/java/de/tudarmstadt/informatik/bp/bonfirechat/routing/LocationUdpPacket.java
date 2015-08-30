e82eb385e152218228ae0ad0f1f73b06977441b4
Johannes Lauinger <johannes@lauinger-it.de>
2015-08-30 05:00:53 +0200

    implements receiving logic in ConnectionManager to update a contact's location

--- BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/routing/LocationUdpPacket.java
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/routing/LocationUdpPacket.java
@@ -9,8 +9,10 @@ import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.UUID;
 
+import de.tudarmstadt.informatik.bp.bonfirechat.data.BonfireData;
 import de.tudarmstadt.informatik.bp.bonfirechat.helper.CryptoHelper;
 import de.tudarmstadt.informatik.bp.bonfirechat.models.Identity;
+import de.tudarmstadt.informatik.bp.bonfirechat.models.Message;
 
 /**
  * Created by johannes on 30.08.15.
@@ -20,15 +22,12 @@ import de.tudarmstadt.informatik.bp.bonfirechat.models.Identity;
  */
 public class LocationUdpPacket extends Packet {
 
-    public final Date sentTime;
-    public byte[] encryptedBody;
-    public byte[] nonce;
-
+    private byte[] encryptedBody;
+    private byte[] nonce;
 
     public LocationUdpPacket(Identity sender, byte[] recipientPublicKey, LatLng location) {
         super(sender.getPublicKey().asByteArray(), recipientPublicKey, UUID.randomUUID());
         this.type = PacketType.LocationUdp;
-        this.sentTime = new Date();
 
         // create location payload
         String body = location.latitude + ":" + location.longitude;
@@ -37,5 +36,11 @@ public class LocationUdpPacket extends Packet {
         Box crypto = new Box(new PublicKey(recipientPublicKey), sender.privateKey);
         nonce = CryptoHelper.generateNonce();
         encryptedBody = crypto.encrypt(nonce, body.getBytes(Charset.forName("UTF-8")));
+    }
+
+    public String getLocation(Identity receiver) {
+        Box crypto = new Box(new PublicKey(senderPublicKey), receiver.privateKey);
+        byte[] body = crypto.decrypt(nonce, encryptedBody);
+        return new String(body, Charset.forName("UTF-8"));
     }
 }
