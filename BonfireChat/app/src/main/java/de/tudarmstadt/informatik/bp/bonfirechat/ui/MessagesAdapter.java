0887dd1541c6b0e78f10d7b26588d1c0ee78f83f
Jonas Mönnig <jonas.moennig@stud.tu-darmstadt.de>
2015-09-01 19:07:28 +0200

    Removed unused import in MessagesAdapter

--- BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/ui/MessagesAdapter.java
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/ui/MessagesAdapter.java
@@ -1,6 +1,5 @@
 package de.tudarmstadt.informatik.bp.bonfirechat.ui;
 
-import android.app.NotificationManager;
 import android.content.Context;
 import android.graphics.Color;
 import android.net.Uri;
